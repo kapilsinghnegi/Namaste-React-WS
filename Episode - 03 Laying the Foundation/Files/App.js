745d08963dc824997ccedd3d009702de4c772973
KSN <kapilsinghnegi@outlook.com>
2024-01-31 22:06:13 +0530

    updated Episode -3

--- Episode - 03 Laying the Foundation/Files/App.js
+++ Episode - 03 Laying the Foundation/Files/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
+import userIcon from "./user.png"; 
 
 // Create a `Nested header Element` using `React.createElement`(h1,h2,h3 inside a div with class "title") 
 // const header = React.createElement(
@@ -38,25 +40,48 @@ import ReactDOM from "react-dom/client";
 // - `Composition of Component` (Add a component inside another)
 // - `{TitleComponent}` vs `{<TitleComponent/>}` vs `{<TitleComponent></TitleComponent>}` in JSX.
 
-const TitleComponent = () => (
-    <div id="container">
-        <h2>Another Component</h2>
-    </div>
-);
-const title = (<h2>An Element</h2>);
+// const TitleComponent = () => (
+//     <div id="container">
+//         <h2>Another Component</h2>
+//     </div>
+// );
+// const title = (<h2>An Element</h2>);
+
+// const Header = () => {
+//     return (
+//         <div className="title">
+//             { title }
+//             <h1 style={{color:"red"}}>This is H1 tag.</h1>
+//             <h2 style={{color:"green"}}>This is H2 tag.</h2>
+//             <h3 style={{color:"blue"}}>This is H3 tag.</h3>
+//             <TitleComponent />
+//             <TitleComponent></TitleComponent>
+//         </div>
+//     )
+// }
+
+// - Create a `Header Component` from scratch using `Functional Component` with JSX
+//   - Add a `Logo on Left`
+//   - Add a `search bar in middle`
+//   - Add `User icon on right`
+//   - Add `CSS to make it look nice`
 
 const Header = () => {
     return (
-        <div className="title">
-            { title }
-            <h1 style={{color:"red"}}>This is H1 tag.</h1>
-            <h2 style={{color:"green"}}>This is H2 tag.</h2>
-            <h3 style={{color:"blue"}}>This is H3 tag.</h3>
-            <TitleComponent />
-            <TitleComponent></TitleComponent>
-        </div>
-    )
-}
+        <header className="header">
+            <div className="left">
+                <img className="logo" src={ logo } alt="Logo" />
+            </div>
+            <div className="center">
+                <input className="input" type="text" placeholder="Search anything you want"/>
+                <button type="submit" className="search">Submit</button>
+            </div>
+            <div className= "right">
+                <img className="icon" src={ userIcon } alt="User Icon"/>
+            </div>
+        </header>
+   );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header />);
